bb8e6fd62e71a3366b50ee2b97c1a2fe75818e29
ElaiasCZ <108530713+ElaiasCZ@users.noreply.github.com>
2024-04-22 10:49:22 -0300

    saves

--- main.js
+++ main.js
@@ -201,14 +201,13 @@ for (let i = 0; i < contadores.length; i++) {
 
 //função para atualizar o cronômetro e no escopo da função (entre chaves) adicionar o laço de repetição.
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-}
     for (let i=0; i<contadores.length;i++){
-       // contadores[i].textContent = calculaTempo(tempos[i]);   
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     }
+}   
 
 //após a declaração da função, chamamos por ela novamente:
 
